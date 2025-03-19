b1db34cf4a260ded676b8fa72bf3f7351bb1580b
SodiniLuca <sodiniluca7@gmail.com>
2025-03-19 13:07:16 +0100

    Matrice_2.2
    Aggiungo un metodo di stampa tramite StringBuilder

--- Matrice.java
+++ Matrice.java
@@ -26,9 +26,26 @@ public class Matrice{
             System.out.println(); 
         }
     }
+
+    /**
+     * Stampa la matrice utilizzando lo String Builder
+     * @param matrice da stampare
+     * @return void
+     */
+    public static void stampaMatriceSB(int [][] m) {
+        // Crea la matrice tramite StringBuilder
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                sb.append(String.format("%4d", m[i][j]));
+            }
+            sb.append(System.lineSeparator()); // Aggiungi una nuova linea alla fine di ogni riga
+        }
+        // Stampa la matrice
+        System.out.print(sb.toString());
+    }
     
     public static void main(String[] args){
-        
         // Costanti di gestione di righe e colonne
         final int righe = 10;
         final int colonne = 10;
@@ -41,6 +58,7 @@ public class Matrice{
 
         // Stampo il contenuto della matrice
         stampaMatrice(m);
-
+        System.out.print("\n");
+        stampaMatriceSB(m);
     }
 }
