e30028e8a01c4c58e08babb3240134ad46d614e4
SodiniLuca <sodiniluca7@gmail.com>
2025-03-19 12:44:35 +0100

    Matrice_2
    Sono stati spostati i codici per popolare e stampare all'interno di due metodi. Tutto è commentato.

--- Matrice.java
+++ Matrice.java
@@ -1,4 +1,32 @@
 public class Matrice{
+    
+    /**
+     * Popola la matrice con i numeri di Pitagora
+     * @param matrice da popolare
+     * @return void
+     */
+    public static void pitagora(int [][] m){
+        for (int i = 0; i < m.length; i++){
+            for (int j = 0; j < m[i].length; j++){
+                m[i][j] = (i + 1) * (j + 1);
+            } 
+        }
+    }
+
+    /**
+     * Stampa la matrice
+     * @param matrice da popolare
+     * @return void
+     */
+    public static void stampaMatrice(int [][] m){
+        for (int i = 0; i < m.length; i++){
+            for (int j = 0; j < m[i].length; j++){
+                System.out.printf("%4d" , m[i][j]);
+            } 
+            System.out.println(); 
+        }
+    }
+    
     public static void main(String[] args){
         
         // Costanti di gestione di righe e colonne
@@ -8,25 +36,11 @@ public class Matrice{
         // Creazione della matrice
         int [][] m = new int [righe][colonne];
 
-        // Popoliamo la matrice con valori (in questo caso si usano i valori di pitagora)
-        for (int i = 0; i < m.length; i++){
-            for (int j = 0; j < m[i].length; j++){
-                m[i][j] = (i + 1) * (j + 1);
-            } 
-        }
+        // Popolo la matrice con i numeri di pitagora
+        pitagora(m);
 
-        // Stampiamo il contenuto della matrice
-        for (int i = 0; i < m.length; i++){
-            for (int j = 0; j < m[i].length; j++){
-                System.out.printf("%4d" , m[i][j]);
-            } 
-            System.out.println(); 
-        }
+        // Stampo il contenuto della matrice
+        stampaMatrice(m);
+
     }
-
-
-
-
-
-
 }
